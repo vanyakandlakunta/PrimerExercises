1daf55fb4609ef627ae0619b47e1278bdd1ab7ae
vanyakandlakunta <vanya.chary@gmail.com>
2015-06-08 00:44:49 +0530

    Solution for Chapter 2 Q1.

--- Chap02/Ex01.cpp
+++ Chap02/Ex01.cpp
@@ -14,4 +14,16 @@
 // =====================================================================================
 
 
-
+/* Write a C++ program that displays your name and address (or if you value your
+ * privacy, a fictitious name and address).*/
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[10] = "vanya";
+    char address[50] = "Door no.10, Harbour Heights, New Delhi, India";
+    std::cout<<"Name:"<<name<<std::endl;
+    std:: cout<<"Address:"<<address<<std::endl;
+    return 0;
+}
